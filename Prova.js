df754d41a7304fb9d516a161000bf6cc665e97ee
Boigdelguin <llucchavarria102@gmail.com>
2025-04-02 11:01:25 +0200

    Add files via upload

---
+++ Prova.js
@@ -0,0 +1,64 @@
+// JS de la pàgina web del Lluc
+/*Multi
+line
+comment*/
+
+function()
+
+// Pop-up
+//alert('Hello world')
+
+document.body.innerHTML = 'text que substitueix';
+
+`template strings`
+
+`Interpolation: ${Veus el que et dic?}``
+
+`Multi 
+line`
+
+
+
+// NÚMEROS
+Math.random() // Crea un número random
+Math.floor() // makes the output a whole number
+Math.round() 
+Math.ceil() // returns the smallest integer greater than or equal to a decimal number
+Number.isInteger() // checks if a number is an integer
+
+//VARIABLES
+let variable1 = 1; //variable
+const variable2 = 2; // constant (default)
+var variable3 = 3; //variable (vella)
+
+//OPERADORS
+// Numèrics
+w += 5; // Can be written as w = w + 5
+x -= 5; // Can be written as x = x - 5
+y *= 2; // Can be written as y = y * 2
+z /= 2; // Can be written as z = z / 2
+a++; // Increment Operator (a = a + 1)
+b--; // Decrement Operator (b = b - 1)
+
+// Booleans
+&&  // And or Guard Operator
+|| // Or or Default Operator)
+! // Not Operator
+?: // Ternary Operator
+
+//COMPARADORS   
+<  // Less than
+> // Greater than
+Less than or equal to: <=
+>=  // Greater than or equal to
+===  // Is equal to
+!==  // Is not equal to
+
+`Hola ${variable}.` // Interpol·lació
+
+typeof variable3 //diu el tipus de variable: número, string...
+
+
+
+
+
---
+++ botiga.js
@@ -0,0 +1,21 @@
+
+// Object
+
+const sessio ={
+    Nom: 'Lluc',
+    Cognoms: 'Chavarria Escriche'
+}
+
+
+
+function funcioCompra() {
+    carro = carro + 1;
+    document.getElementById('Carret').innerHTML = `${carro}`;
+}
+
+function substitueixCarro() {
+    document.getElementById('Carret').innerHTML = `${carro}`;
+}
+
+
+
